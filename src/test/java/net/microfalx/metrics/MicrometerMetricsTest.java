850c83e87763087c9037ab25ea5d7bd45b6d0ba9
Adrian Tarau <adrian.tarau@gmail.com>
2025-02-10 09:21:42 -0500

    Fix tests

--- src/test/java/net/microfalx/metrics/MicrometerMetricsTest.java
+++ src/test/java/net/microfalx/metrics/MicrometerMetricsTest.java
@@ -1,7 +1,12 @@
 package net.microfalx.metrics;
 
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import net.microfalx.lang.ThreadUtils;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -12,6 +17,8 @@ class MicrometerMetricsTest {
 
     @BeforeAll
     static void initializeMicrometerMetrics() {
+        io.micrometer.core.instrument.Metrics.globalRegistry.clear();
+        io.micrometer.core.instrument.Metrics.addRegistry(new SimpleMeterRegistry());
         micrometerMetrics = new MicrometerMetrics("Test");
     }
 
@@ -85,5 +92,20 @@ class MicrometerMetricsTest {
         assertNotNull(timer.getLastAccess());
         assertEquals("test2.t2", timer.getName());
         assertEquals(7d, timer.record(() -> 7d));
+    }
+
+    @Test
+    void getTimersFromGroup() {
+        Metrics metrics = Metrics.of("Test");
+        metrics.time("c1", (t) -> ThreadUtils.sleepMillis(5));
+        metrics.time("c2", t -> ThreadUtils.sleepMillis(5));
+        metrics.timeCallable("c3", () -> {
+            ThreadUtils.sleepMillis(5);
+            return null;
+        });
+        assertEquals(3, metrics.getTimers().size());
+        Assertions.assertThat(metrics.getTimers().stream().map(Timer::getDuration)
+                        .reduce(Duration.ZERO, Duration::plus).toMillis())
+                .isGreaterThan(1);
     }
 }
